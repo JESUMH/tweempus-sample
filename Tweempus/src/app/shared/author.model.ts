23a0fe7ede8deee6ae0d673d9d3a64d628a5dc14
JESUS MARTINEZ HERRERO <jesus.martinez@bionet.com>
2021-06-08 11:21:01 +0200

    Modulo 3 enlace de datos

---
+++ Tweempus/src/app/shared/author.model.ts
@@ -0,0 +1,41 @@
+export class Author {
+  private _id: string;
+  private _url: string;
+  private _fullName: string;
+  private _image: string;
+
+  constructor(id: string) {
+    this._id = id;
+    this._url = '';
+    this._fullName = 'Author';
+    this._image = 'resources/images/google-icon.png';
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+  public set id(value: string) {
+    this._id = value;
+  }
+
+  public get url(): string {
+    return this._url;
+  }
+  public set url(value: string) {
+    this._url = value;
+  }
+
+  public get fullName(): string {
+    return this._fullName;
+  }
+  public set fullName(value: string) {
+    this._fullName = value;
+  }
+
+  public get image(): string {
+    return this._image;
+  }
+  public set image(value: string) {
+    this._image = value;
+  }
+}
---
+++ Tweempus/src/app/shared/twimp/twimp.model.ts
@@ -0,0 +1,58 @@
+import { Author } from '../author.model';
+
+export class Twimp {
+  private _id: string;
+  private _url: string;
+  private _author: Author;
+  private _content: string;
+  private _timestamp: string;
+  private _favorite: boolean;
+
+  constructor(id: string, url: string, author: Author, content: string, timestamp: string, favorite: boolean) {
+    this._id = id;
+    this._url = url;
+    this._author = author;
+    this._content = content;
+    this._timestamp = timestamp;
+    this._favorite = favorite;
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+  public set id(value: string) {
+    this._id = value;
+  }
+  public get url(): string {
+    return this._url;
+  }
+  public set url(value: string) {
+    this._url = value;
+  }
+
+  public get author(): Author {
+    return this._author;
+  }
+  public set author(value: Author) {
+    this._author = value;
+  }
+
+  public get content(): string {
+    return this._content;
+  }
+  public set content(value: string) {
+    this._content = value;
+  }
+  public get timestamp(): string {
+    return this._timestamp;
+  }
+  public set timestamp(value: string) {
+    this._timestamp = value;
+  }
+  public get favorite(): boolean {
+    return this._favorite;
+  }
+  public set favorite(value: boolean) {
+    this._favorite = value;
+  }
+}
